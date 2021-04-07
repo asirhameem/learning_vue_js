c41c1829cf82ae3c64fe1d2ba87cfe818d84cbbb
Asir Hameem <56108077+asirhameem@users.noreply.github.com>
2021-04-07 19:41:18 +0600

    Directives and Binding

--- gene_vue/genesis.js
+++ gene_vue/genesis.js
@@ -1,7 +1,8 @@
-let obj = new Vue({
+let divOne = new Vue({
     el: '#first',
     data: {
-        name: "Asir Hameem"
+        name: "Asir Hameem",
+        you: "Hey, Who are you ?"
     },
     methods: {
         GetName() {
@@ -9,4 +10,25 @@ let obj = new Vue({
         }
     },
     template: '<div> <h2>My name is : {{ GetName() }}</h2></div>'
-})
+});
+
+let divTwo = new Vue({
+    el: '#second',
+    data: {
+        address: "Rajapukur Lane, Chittagong",
+        phone: "01521400829",
+        university: "AIUB",
+        facebook: "https://www.facebook.com/asirhameem/",
+        twitter: "https://www.twitter.com/asirhameem/"
+
+    },
+    methods: {
+        UpdatePhone(number) {
+            this.phone = number;
+            return this.phone;
+        },
+        GetAddress() {
+            return this.address;
+        }
+    }
+});
