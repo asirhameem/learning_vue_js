cf3c0e3b50437e875932b1e64bfd9417c750fec3
Asir Hameem <asirhameem@gmail.com>
2021-05-24 14:39:36 +0600

    Vue and Asp.Net Core First Application

---
+++ Vue_Mvc/Vue_Mvc/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Vue_Mvc.Models;
+
+namespace Vue_Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private DataContext context;
+        public HomeController(ILogger<HomeController> logger, DataContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+       public IActionResult Login(User user) 
+        {
+            var userInfo = context.Users.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault();
+            if(userInfo == null)
+            {
+                return View();
+            }else if (userInfo.Email == user.Email && userInfo.Password == user.Password)
+            {
+                return Ok();
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Vue_Mvc/Vue_Mvc/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vue_Mvc.Models;
+
+namespace Vue_Mvc.Repository
+{
+    public class UserRepository 
+    {
+        
+        public UserRepository()
+        {
+
+        }
+    }
+}
